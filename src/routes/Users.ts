93ceac74d672f1f708938634172d8846b404a6b0
wswilliams <wswilliamssilva938@gmail.com>
2021-05-13 21:18:33 -0300

    Add users

--- src/routes/Users.ts
+++ src/routes/Users.ts
@@ -22,22 +22,22 @@ router.post('/add-user-system', async (req: IRequest, res: Response) => {
         });
     }
     await userDao.addLogin(user);
-    return res.status(CREATED).end();
+    return res.status(CREATED).json({mensage: "Usuário criado com sucesso"});
 });
 
 /******************************************************************************
  *                       Add One - "POST /api/users/add"
  ******************************************************************************/
 
-router.post('/add-user', async (req: IRequest, res: Response) => {
+router.post('/add-user', async (req: Request, res: Response) => {
     const { user } = req.body;
     if (!user) {
         return res.status(BAD_REQUEST).json({
             error: paramMissingError,
         });
     }
-    const register = await userDao.addUser(user);
-    return res.status(CREATED).json(register);
+    await userDao.addUser(user);
+    return res.status(CREATED).json({mensage: "Arquivo criado com sucesso"});
 });
 
 
